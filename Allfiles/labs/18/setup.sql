4c23b6470483ad36cf5c05f8ef6bbbc911e7b7e0
41L3X M@Raya <82734863+xpython21@users.noreply.github.com>
2023-07-12 20:21:46 +0200

    Expand table

--- Allfiles/labs/18/setup.sql
+++ Allfiles/labs/18/setup.sql
@@ -2,6 +2,9 @@ CREATE TABLE dbo.FactOrder
 (
     OrderDateTime datetime,
     ProductID int,
-    Quantity int
+    Quantity int,
+    StoreID int,
+    EmployeeName string,
+    USState string
 )
 GO
